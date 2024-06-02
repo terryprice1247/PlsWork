b8625d06c34dbc231f8421dce58b7f4a05752f20
price <pricet81@yahoo.com>
2024-06-02 08:53:40 -0700

    Implement "Add new todo" and "New items" feature

--- WebApp-Todo/webb.py
+++ WebApp-Todo/webb.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos()                # establishing a variable in our function
+todos = functions.get_todos()  # establishing a variable in our function
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"   # session state allows us to extract from input the n makes new line
+    todos.append(todo)
+    functions.write_todos(todos)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app. ")
 st.write("This app is to increase your productivity")
 
-for todo in todos:                          # calling the function to make a checkbox for the todos
+for todo in todos:  # calling the function to make a checkbox for the todos
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")      # A placeholder for the textbox giving a hint
+st.text_input(label="Enter Here", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')  # A placeholder for the textbox giving a hint
+
+st.session_state
